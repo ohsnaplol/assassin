d43634711656248df9e087286788789c12908a41
ohsnaplol <emyb@bellsouth.net>
2018-02-17 13:36:52 -0500

    added routes for browse admin and player pages

--- controllers/assassin_controller.js
+++ controllers/assassin_controller.js
@@ -5,4 +5,16 @@ router.get('/', function(request, response) {
   response.render('index')
 })
 
+router.get('/browse', function(request, response) {
+  response.render('gamebrowser')
+})
+
+router.get('/admin', function(request, response) {
+  response.render('admin')
+})
+
+router.get('/player', function(request, response) {
+  response.render('player')
+})
+
 module.exports = router
