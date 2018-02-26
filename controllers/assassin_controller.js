c79250b9746bb19cf6d1cb1eba8ed0ef3908bb8b
ohsnaplol <emyb@bellsouth.net>
2018-02-26 03:22:09 -0500

    redirect index to browser. admin view, start game, add player routes

--- controllers/assassin_controller.js
+++ controllers/assassin_controller.js
@@ -3,11 +3,7 @@ var router = express.Router()
 var db = require('../models')
 
 router.get('/', function (request, response) {
-  response.render('index')
-})
-
-router.get('/signup', function (request, response) {
-  response.render('signup')
+  response.redirect('browse')
 })
 
 router.get('/browse', function (request, response) {
@@ -36,12 +32,105 @@ router.get('/game/id/:id', function (request, response) {
   })
 })
 
-router.get('/admin', function (request, response) {
-  response.render('admin')
+router.get('/admin/:id', function (request, response) {
+  console.log('/admin/:id CALLED')
+  console.log('REQUEST.PARAMS.ID = ' + request.params.id)
+  var gameId = request.params.id
+  var hbsObject = {}
+  db.Game.findOne({
+    where: {
+      id: gameId
+    }
+  }).then(function(dbGame) {
+    db.Player.findAll({
+      where: {
+        GameId: gameId
+      }
+    }).then(function(dbPlayer) {
+      hbsObject = {
+        game: dbGame,
+        player: dbPlayer
+      }
+      console.log('render admin page with ' + JSON.stringify(hbsObject))
+      response.render('admin', hbsObject)
+    })
+  })
 })
 
 router.get('/player', function (request, response) {
   response.render('player')
+})
+
+router.post('/api/:id/authenticatePlayer', function(request,response) {
+  var inputPlayerPass = request.body.password
+  var name = request.body.name
+
+})
+
+router.post('/api/authenticateAdmin/:id', function(request, response) {
+  var inputAdminPass = request.body.adminpassword
+  var gameId = request.params.id
+  db.Game.findOne({
+    where: {
+      id: gameId
+    }
+  }).then(function(dbGame) {
+    if(inputAdminPass == dbGame.adminPassword) {
+      response.redirect(`/admin/${dbGame.id}`)
+    }
+  })
+})
+
+router.put('/api/startGame/:id', function(request, response) {
+  console.log('PUT CALLED')
+  var gameid = request.params.id
+  db.Game.update({
+    gameIsActive: true
+  }, {
+    where: {
+      id: gameid
+    }
+  }).then(function(dbGame) {
+    console.log('THEN SUCCESS! GameId = ' + gameid)
+    response.redirect(`back`)
+  })
+})
+
+router.post('/api/addPlayer/:toGameWithid', function(request, response) {
+  var inputName = request.body.name
+  var playerPass = request.body.password
+  var gamePass = request.body.gamepasscode
+  var gameid = request.params.toGameWithid
+  db.Game.findOne({
+    where: {
+      id: gameid
+    }
+  }).then(function(dbGame) {
+    // check if player already exists
+    db.Player.findOne({
+      where: {
+        GameId: gameid,
+        name: inputName
+      }
+    }).then(function(entry) {
+      if(entry == null) {
+        console.log('entry == null')
+        // check if passcode is valid
+        if(gamePass == dbGame.password) {
+          db.Player.create({
+            name: inputName,
+            password: playerPass,
+            GameId: gameid
+          }).then(function(result) {
+            response.redirect(`/game/id/${gameid}`)
+          })
+        }
+      } else {
+        console.log('entry already exists')
+        return
+      }
+    })
+  })
 })
 
 router.post('/api/creategame', function(request, response) {
