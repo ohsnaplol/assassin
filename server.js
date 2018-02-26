fdb2f40ee9297a13bcf86eb7511ff4598bae5d87
ohsnaplol <emyb@bellsouth.net>
2018-02-26 03:20:56 -0500

    removed semicolons and fixed static directory issue

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const bodyParser = require("body-parser");
-const exphbs  = require('express-handlebars');
-const app = express();
+const express = require('express')
+const bodyParser = require("body-parser")
+const exphbs  = require('express-handlebars')
+const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-app.use(express.static('public'))
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.use(express.static(__dirname + '/public'))
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
 
-var db = require("./models");
-var routes = require("./controllers/assassin_controller.js");
-app.use("/", routes);
+var db = require("./models")
+var routes = require("./controllers/assassin_controller.js")
+app.use("/", routes)
 
 db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, ()=> {
